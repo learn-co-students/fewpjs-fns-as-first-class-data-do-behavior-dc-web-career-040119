507880ad401a1a57a26636aabf03f32d77be62ec
Matthew <mf9646a@alumni.american.edu>
2019-05-13 12:21:34 -0400

    Done.

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function greet(time) {
 
 /* Write your implementation of displayMessage() */
 
-function displayMessage(myString) {
-
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
 }
