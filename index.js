c332a0a4a69a075459caac5ea15e34d393805748
Matthew <mf9646a@alumni.american.edu>
2019-05-13 12:20:37 -0400

    passed 3/4 tests. Need to see solution for last one

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  if (time[0] == 0 || time[1] == ":" || time[2] == ":" && time[1] < 2) {
+    return 'Good Morning'
+  }
+  else if (time[2] == ":" && time[1] > 2 && time[1] < 7) {
+    return 'Good Afternoon'
+  }
+  else if (time[2] == ":" && time[0] == 1 && time[1] > 6 || time[0] == 2 ) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(myString) {
+
+}
