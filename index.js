bea1d65fe9a7c90a83b9acc208c54ff29790e0a7
Daniel Hallada <hallada.daniel@gmail.com>
2019-05-13 12:41:48 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(string) {
+	const date_time = (parseInt(string.split(":").join("")))
+
+	if (date_time < 1200) {
+		return "Good Morning"
+	}
+	else if (date_time >= 1200 && date_time <=1700) {
+		return "Good Afternoon"
+	}
+	else {
+		return "Good Evening"
+	}
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+	document.getElementById('greeting').textContent = string
+	debugger
+}
